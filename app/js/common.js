60c21d6a3b8e03edca92abf8f0248eca2663b35a
alik0211 <alik0211alik@gmail.com>
2017-06-18 20:52:18 +0400

    Fix codestyle

--- app/js/common.js
+++ app/js/common.js
@@ -1,18 +1,5 @@
 let userCity = localStorage.getItem('city');
 
-if (userCity === null) {
-  toggleDialog(true);
-
-  document.querySelector('.button').addEventListener('click', function() {
-    userCity = document.getElementById('city').value;
-    toggleDialog(false);
-    getWeather(userCity).then(data => updateWeather(data, userCity));
-    localStorage.setItem('city', userCity);
-  });
-} else {
-  getWeather(userCity).then(data => updateWeather(data, userCity));
-}
-
 if ('caches' in window) {
   caches.match('https://alik0211.tk/weather0211/data.php?city=' + userCity)
     .then(function(response) {
@@ -22,6 +9,19 @@ if ('caches' in window) {
         });
       }
     });
+}
+
+if (userCity === null) {
+  toggleDialog(true);
+
+  document.querySelector('.button').addEventListener('click', function() {
+    userCity = document.getElementById('city').value;
+    toggleDialog(false);
+    getWeather(userCity).then(data => updateWeather(data, userCity));
+    localStorage.city = userCity;
+  });
+} else {
+  getWeather(userCity).then(data => updateWeather(data, userCity));
 }
 
 function toggleDialog(visible) {
@@ -43,13 +43,18 @@ function updateWeather(data, city) {
   const card = document.querySelector('.card');
 
   card.querySelector('.card__location').textContent = city;
-  card.querySelector('.card__description').textContent = data.weather[0].description;
+  card.querySelector('.card__description').textContent =
+    data.weather[0].description;
 
   card.querySelector('.visual__value').textContent = Math.round(data.main.temp);
-  card.querySelector('.visual__icon').style.backgroundImage = `url("images/${data.weather[0].icon}.svg")`;
-  card.querySelector('.description__wind .description__value').textContent = Math.round(data.wind.speed);
-  card.querySelector('.description__clouds .description__value').textContent = data.clouds.all;
-  card.querySelector('.description__pressure .description__value').textContent = Math.round(data.main.pressure);
+  card.querySelector('.visual__icon').style.backgroundImage =
+    `url("images/${data.weather[0].icon}.svg")`;
+  card.querySelector('.description__wind .description__value').textContent =
+    Math.round(data.wind.speed);
+  card.querySelector('.description__clouds .description__value').textContent =
+    data.clouds.all;
+  card.querySelector('.description__pressure .description__value').textContent =
+    Math.round(data.main.pressure);
 
   document.querySelector('.loader').setAttribute('hidden', true);
 }
